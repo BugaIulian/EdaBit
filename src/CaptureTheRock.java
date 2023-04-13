b01fe4081636cf2706cff82659790049c9dec0ee
BugaIulian <iulian.ionut.buga@gmail.com>
2023-04-13 23:11:05 +0300

    first commit

--- src/CaptureTheRock.java
+++ src/CaptureTheRock.java
@@ -6,7 +6,6 @@ public abstract class CaptureTheRock {
         System.out.println(newTestString.substring(2));
         System.out.println(canCapture(rockArray));
     }
-
     private static boolean canCapture(String[] rockArray) {
         if (rockArray[0].substring(0, 1).equals(rockArray[1].substring(0, 1))) {
             return true;
